e9f73379375ede1e7b2e59a06516a9e36582dcd9
EvgVasilev <vasilevevgur@rambler.ru>
2024-10-31 12:58:41 +0300

    test

---
+++ wa-apps/installer/lib/config/app.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+    'name'        => 'Installer',
+    'description' => 'Install new apps from the Webasyst Store',
+    'icon'        => 'img/installer.svg',
+    'mobile'      => false,
+    'version'     => '3.2.0',
+    'critical'    => '3.2.0',
+    'system'      => true,
+    'vendor'      => 'webasyst',
+    'csrf'        => true,
+    'ui'          => '1.3,2.0'
+);
